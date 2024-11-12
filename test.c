534b962248e15c021d5ea52a1b9687c2384e9838
Abhishek Kumar <146948267+Abhishekpkd@users.noreply.github.com>
2024-11-13 01:44:50 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void loopInvariantExample(int n, int *array) {
+    int a = 500;
+    int b = 10000;
+    int c = a * b; // Loop-invariant computation
+
+    for (int i = 0; i < n; ++i) {
+        array[i] = c + i; // `c * i` is not invariant, but `c` is
+    }
+}
+
+int main() {
+    const int size = 10000;
+    int array[size];
+
+    loopInvariantExample(size, array);
+
+    for (int i = 0; i < size; ++i) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
